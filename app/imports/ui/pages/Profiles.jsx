c656a23e9d671d6c6515432a49e884cb53f6e900
junjieyu808 <junjieyu@hawaii.edu>
2024-05-03 19:46:11 -1000

    Profiles

--- app/imports/ui/pages/Profiles.jsx
+++ app/imports/ui/pages/Profiles.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { Container, Row, Col, Card, Image } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
@@ -6,11 +8,9 @@ import { Profiles } from '../../api/profiles/Profiles';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { PageIDs } from '../utilities/ids';
 import { Recipes } from '../../api/recipes/Recipes';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
 
-function getRecipeData(recipeName) {
-  const recipe = Recipes.collection.findOne({ name: recipeName });
+function getRecipeData(recipeName, email) {
+  const recipe = Recipes.collection.findOne({ name: recipeName, email });
   return recipe;
 }
 
@@ -43,15 +43,14 @@ const ProfilesPage = () => {
     const sub1 = Meteor.subscribe(Profiles.userPublicationName);
     const email = Meteor.user().emails[0].address;
     const profile = Profiles.collection.findOne({ email });
-    const recipes = Recipes.collection.findOne({ email });
-    const recipeData = recipes.map(recipe => getRecipeData(recipe.name));
 
     return {
       ready: sub1.ready(),
       profile,
-      //recipes
     };
   }, []);
+
+  const userRecipes = profile ? profile.projects.map(project => getRecipeData(project, profile.email)).filter(recipe => recipe) : [];
 
   return ready ? (
     <Container id={PageIDs.profilesPage}>
@@ -64,24 +63,30 @@ const ProfilesPage = () => {
         {profile && (
           <React.Fragment>
             <Col md={6}>
-              <div class="m-3">
+              <div className="m-3">
                 <img src={profile.picture} alt={`${profile.firstName} ${profile.lastName}`} width={300}/>
               </div>
-              <div class="pt-4 m-5">
+              <div className="pt-4 m-5">
                 <h2>{profile.firstName} {profile.lastName}</h2>
               </div>
-              <div class="pt-4">
+              <div className="pt-4">
                 <p>{profile.bio}</p>
               </div>
             </Col>
             <Col md={6}>
-              <div class="pt-2">
+              <div className="pt-2">
                 <h2>My Recipes</h2>
-                  <Row xs={1} md={2} lg={4} className="g-2">
-                    {recipeData.map((recipe, index) => <MakeCard key={index} recipe={recipe} />)}
+                {userRecipes.length > 0 ? (
+                  <Row>
+                    {userRecipes.map(recipe => (
+                      <MakeCard key={recipe._id} recipe={recipe} />
+                    ))}
                   </Row>
+                ) : (
+                  <p>No recipes found.</p>
+                )}
               </div>
-              <div class="pt-4">
+              <div className="pt-4">
                 <h2>Favorite Recipes</h2>
 
               </div>
