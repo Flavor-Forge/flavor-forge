dc6ced3ba203ece11c3518b36f1a750ae456c733
Weibin Wu <weibinwu@hawaii.edu>
2024-05-07 00:59:29 -1000

    Update

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -17,12 +17,12 @@ import Profiles from '../pages/Profiles';
 import Interests from '../pages/Interests';
 import Filter from '../pages/Filter';
 import HomePage from '../pages/HomePage';
+import EditRecipePage from '../pages/EditRecipePage';
 import RecipeListPage from '../pages/RecipeListPage';
 import RecipePage from '../pages/RecipePage';
 import AddRecipe from '../pages/AddRecipe';
 import LoadingSpinner from '../components/LoadingSpinner';
 import RecipeListPageAdmin from '../pages/RecipeListPageAdmin';
-import EditRecipePage from '../pages/EditRecipePage';
 
 /* Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 const App = () => {
@@ -44,9 +44,9 @@ const App = () => {
           <Route path="/interests" element={<Interests />} />
           <Route path="/profiles" element={<ProtectedRoute><Profiles /></ProtectedRoute>} />
           <Route path="/recipelistpage" element={<RecipeListPage />} />
+          <Route path="/edit-recipe/:recipeId" element={<ProtectedRoute><EditRecipePage /></ProtectedRoute>} />
           <Route path="/recipelistpageadmin" element={<AdminProtectedRoute ready={ready}><RecipeListPageAdmin /></AdminProtectedRoute>} />
           <Route path="/recipe/:recipeId" element={<RecipePage />} />
-          <Route path="/edit-recipe/:recipeId" element={<ProtectedRoute><EditRecipePage /></ProtectedRoute>} />
           <Route path="/home" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
           <Route path="/filter" element={<ProtectedRoute><Filter /></ProtectedRoute>} />
           <Route path="/addrecipe" element={<ProtectedRoute><AddRecipe /></ProtectedRoute>} />
